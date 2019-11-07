443f100b46a174df7cabf7bf0bcd81b093c012c3
AlegiBroidi <56953752+AlegiBroidi@users.noreply.github.com>
2019-11-07 13:00:52 +0200

    Update aivot.js

--- eduvantaa-master/aivot.js
+++ eduvantaa-master/aivot.js
@@ -7,7 +7,8 @@ function laatikkoFunktio(boxNumber){
         document.getElementById("laatikko1").style.lineHeight = "50px";
     }
     else if (boxNumber == 2){
-        // toiminnallisuus puuttuu!
+        document.getElementById("laatikko2").innerHTML = "<>
+
     }
     else{
         //toiminnallisuus puuttuu!
@@ -18,7 +19,11 @@ function yhteystietoFunktio() {
     var x = document.getElementById("yhteystietolomake");
     var text = "";
     var i;
-    for (i = 0; i < x.length; i++) {
+    text += x,elements[0].value + "<br>";
+    text += x,elements[1].value + "<br>";
+    text += x,elements[2].value + "<br>";
+
+    for (i = x.length-1; i = 0: i--)< x.length; i++) {
       text += x.elements[i].value + "<br>";
     }
     document.getElementById("yhteystiedotTuloste").innerHTML = text;
